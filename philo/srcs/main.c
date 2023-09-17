cc4679d1f6c2fc44d427f0a31835f340098db8ff
Mori062 <01morishi@gmail.com>
2023-09-17 22:41:41 +0900

    init

---
+++ philo/srcs/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 18:36:17 by morishitash       #+#    #+#             */
+/*   Updated: 2023/09/17 18:52:10 by morishitash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	printf("I'm philo.\n");
+	return (0);
+}
---
+++ philo_bonus/srcs/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 18:36:17 by morishitash       #+#    #+#             */
+/*   Updated: 2023/09/17 18:52:07 by morishitash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo_bonus.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	printf("I'm philo_bonus.\n");
+	return (0);
+}
